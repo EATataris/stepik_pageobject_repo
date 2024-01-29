1895f3528eaf0d86855b55223eaad9948dc7f216
EATataris <emir-ali818@mail.ru>
2024-01-29 20:59:50 +0300

    First commit message for module 4

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Choose language')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    request.config.getoption("language")
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(10)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
