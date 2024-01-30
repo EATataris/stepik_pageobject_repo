600f687716055fe749243d619eba34f8d89069fe
EATataris <emir-ali818@mail.ru>
2024-01-30 20:59:50 +0300

    add testing of basket button

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_NAME_MESSAGE = (By.CSS_SELECTOR, ".fade.in:nth-child(1) strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main > h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main > p")
+    BASKET_PRODUCT_PRICE = (By.CSS_SELECTOR, ".fade.in:nth-child(3) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,32 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+import time
+
+
+class ProductPage(BasePage):
+
+    def could_add_to_basket(self):
+        self.should_be_basket_button()
+        self.add_to_basket()
+        # self.solve_quiz_and_get_code()
+        self.name_of_product_check()
+
+    def add_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket_button.click()
+        self.solve_quiz_and_get_code()
+        # alert = self.browser.switch_to.alert
+        # alert.accept()
+
+    def should_be_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), "Add to basket button doesn't exist"
+
+    def name_of_product_check(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_MESSAGE).text
+        assert  product_name == product_name_message, 'Name of added book is wrong!'
+
+    def price_of_product_check(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_product_price = self.browser.find_element(*ProductPageLocators.BASKET_PRODUCT_PRICE).text
+        assert product_price == basket_product_price, 'Price is wrong!'
