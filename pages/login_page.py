717a3782859f5529d8e40ba84d1937d05aefceb6
EATataris <emir-ali818@mail.ru>
2024-01-30 13:53:38 +0300

    add checks for login page

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
+        login_link.click()
+        # MainPage.go_to_login_page(self)
+        assert 'login' in self.browser.current_url, 'login_url must have "login" inside!!'
+
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "login form doesn't exist at this page"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "registration form does't exist at this page"
